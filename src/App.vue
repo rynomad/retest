a772b0f92917accd82bd02f8798f8e35b9a5316b
Ryan Bennett <nomad.ry@gmail.com>
2023-08-15 15:18:09 -0600

    initial rete-kit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Welcome to Your Vue.js + Rete.js App</h1>
+    <div class="rete" ref="rete"></div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { createEditor } from './rete';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  async mounted() {
+    await createEditor(this.$refs.rete as HTMLElement)
   }
 });
 </script>
@@ -23,5 +26,17 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.rete {
+  position: relative;
+  width: 80vw;
+  height: 90vh;
+  font-size: 1rem;
+  background: white;
+  margin: 1em auto;
+  border-radius: 1em;
+  text-align: left;
+  border: 3px solid #55b881;
 }
 </style>
